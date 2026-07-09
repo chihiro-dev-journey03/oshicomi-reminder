54a5656e5c828d5aedd71337b94f5984aff98bcb
Chihiro <bcke862354@gmail.com>
2026-07-09 20:45:11 +0900

    feat: OmniAuthを使ったGoogleログイン機能を実装する
    - omniauth-google-oauth2 gemを追加
    - devise.rbにGoogle OmniAuth設定を追加
    - Userモデルにfind_or_create_from_google_omniauthメソッドを実装
      - provider/uidで既存ユーザーを検索
      - 同一メールアドレスの既存ユーザーにGoogle連携を紐付け
      - 新規ユーザーはGoogleの実メールアドレスで作成
    - OmniauthCallbacksControllerにgoogle_oauth2アクションを追加
    - 新規登録・ログインページにGoogleボタン（青）を追加
    
    closes #82

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,6 +10,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.omniauth :line, ENV["LINE_CHANNEL_ID"], ENV["LINE_CHANNEL_SECRET"], scope: "profile openid"
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
